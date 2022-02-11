292dd0352e61cf912d2efb4e94755d4d4499b7fd
kennard-yea <chrismdollinger@gmail.com>
2022-02-11 11:31:33 -0500

    Updated helpText

--- bash-pgscripts/postgres-functions.bash
+++ bash-pgscripts/postgres-functions.bash
@@ -9,7 +9,7 @@ function Get-PgVersion {
   \r\r NAME
   \r    Get-PgVersion
   \r\n DESCRIPTION
-  \r    Connects to a database cluster and echos the major release of PostgreSQL running at a connection string. Relies on the environment variables PGHOST, PGPORT, PGDATABASE, and PGUSER for connection
+  \r    Connects to a database cluster and echos its major version number. Relies on the environment variables PGHOST, PGPORT, PGDATABASE, and PGUSER for connecting
   \r\n USAGE
   \r    Get-PgVersion
   \r\n GLOBALS
@@ -19,7 +19,7 @@ function Get-PgVersion {
   \r\n OUTPUTS
   \r    Outputs the the major release of PostgreSQL stored in the PG_VERSION file of the database cluster
   \r\n RETURNS
-  \r    The expected return status of psql. Returns 0 to the shell if it finished normally, 1 if a fatal error of its own occurs (e.g., out of memory, file not found), 2 if the connection to the server went bad and the session was not interactive.
+  \r    The return status of the psql command. Returns 0 if it finished normally, 1 if a fatal error of its own occurs (e.g., out of memory, file not found), 2 if the connection to the server went bad and the session was not interactive.
   \r\n'
   [[ ${1} == '--help' ]] && printf "${helpText}" && return 1
 
@@ -42,7 +42,7 @@ function Get-PgClusterName {
   \r\n OUTPUTS
   \r    Outputs the the current setting of cluster_name. If cluster_name is not set or if the verson of PostgreSQL being connected to does not support cluster_name, output null.
   \r\n RETURNS
-  \r    The expected return status of psql. Returns 0 to the shell if it finished normally, 1 if a fatal error of its own occurs (e.g., out of memory, file not found), 2 if the connection to the server went bad and the session was not interactive.
+  \r    The return status of the psql command. Returns 0 if it finished normally, 1 if a fatal error of its own occurs (e.g., out of memory, file not found), 2 if the connection to the server went bad and the session was not interactive.
   \r\n'
   [[ ${1} == '--help' ]] && printf "${helpText}" && return 1
 
