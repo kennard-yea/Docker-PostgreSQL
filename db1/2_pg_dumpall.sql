66e986cf275a75182f28818327462c516e617bb4
Christopher Dollinger <chrismdollinger@gmail.com>
2022-03-20 15:33:59 -0400

    Updated pg_dumpall schema script

--- db1/2_pg_dumpall.sql
+++ db1/2_pg_dumpall.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database cluster dump
 --
 
--- Started on 2022-03-06 22:33:14 UTC
+-- Started on 2022-03-20 19:29:31 UTC
 
 SET default_transaction_read_only = off;
 
@@ -42,9 +42,9 @@ GRANT pg_monitor TO grafana GRANTED BY postgres;
 --
 
 -- Dumped from database version 14.2 (Debian 14.2-1.pgdg110+1)
--- Dumped by pg_dump version 14.1
+-- Dumped by pg_dump version 14.2
 
--- Started on 2022-03-06 22:33:14 UTC
+-- Started on 2022-03-20 19:29:31 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -57,7 +57,7 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
--- Completed on 2022-03-06 22:33:14 UTC
+-- Completed on 2022-03-20 19:29:31 UTC
 
 --
 -- PostgreSQL database dump complete
@@ -72,9 +72,9 @@ SET row_security = off;
 --
 
 -- Dumped from database version 14.2 (Debian 14.2-1.pgdg110+1)
--- Dumped by pg_dump version 14.1
+-- Dumped by pg_dump version 14.2
 
--- Started on 2022-03-06 22:33:14 UTC
+-- Started on 2022-03-20 19:29:31 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -88,7 +88,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 3338 (class 1262 OID 16386)
+-- TOC entry 3339 (class 1262 OID 16386)
 -- Name: grafana; Type: DATABASE; Schema: -; Owner: grafana
 --
 
@@ -111,7 +111,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 7 (class 2615 OID 16387)
+-- TOC entry 6 (class 2615 OID 16387)
 -- Name: pglogs; Type: SCHEMA; Schema: -; Owner: grafana
 --
 
@@ -121,7 +121,7 @@ CREATE SCHEMA pglogs;
 ALTER SCHEMA pglogs OWNER TO grafana;
 
 --
--- TOC entry 6 (class 2615 OID 16388)
+-- TOC entry 5 (class 2615 OID 16388)
 -- Name: pgmetrics; Type: SCHEMA; Schema: -; Owner: grafana
 --
 
@@ -129,16 +129,6 @@ CREATE SCHEMA pgmetrics;
 
 
 ALTER SCHEMA pgmetrics OWNER TO grafana;
-
---
--- TOC entry 5 (class 2615 OID 16389)
--- Name: pgstats_history; Type: SCHEMA; Schema: -; Owner: grafana
---
-
-CREATE SCHEMA pgstats_history;
-
-
-ALTER SCHEMA pgstats_history OWNER TO grafana;
 
 --
 -- TOC entry 2 (class 3079 OID 16390)
@@ -149,7 +139,7 @@ CREATE EXTENSION IF NOT EXISTS file_fdw WITH SCHEMA public;
 
 
 --
--- TOC entry 3339 (class 0 OID 0)
+-- TOC entry 3340 (class 0 OID 0)
 -- Dependencies: 2
 -- Name: EXTENSION file_fdw; Type: COMMENT; Schema: -; Owner: 
 --
@@ -158,7 +148,46 @@ COMMENT ON EXTENSION file_fdw IS 'foreign-data wrapper for flat file access';
 
 
 --
--- TOC entry 2036 (class 1417 OID 16394)
+-- TOC entry 229 (class 1255 OID 16822)
+-- Name: refresh_wal_metrics(); Type: PROCEDURE; Schema: pgmetrics; Owner: grafana
+--
+
+CREATE PROCEDURE pgmetrics.refresh_wal_metrics()
+    LANGUAGE sql
+    AS $$INSERT INTO pgmetrics.wal_metrics (
+WITH jtr AS (
+    SELECT
+        id
+        , metrics_raw -> 'wal' AS wal
+    FROM
+        pgmetrics.raw_metrics rm
+	WHERE rm.collection_at > (select max(wm.collection_at) from pgmetrics.wal_metrics wm)
+	OR rm.cluster_name not in (select DISTINCT wm.cluster_name from pgmetrics.wal_metrics wm)
+)
+SELECT
+    rm.collection_at
+    , rm.cluster_name
+    , wal.*
+FROM
+    jtr
+    LEFT JOIN pgmetrics.raw_metrics rm ON jtr.id = rm.id
+    , LATERAL jsonb_to_record(jtr.wal) AS wal (fpi int
+        , sync int
+        , bytes int
+        , write int
+        , records int
+        , sync_time int
+        , write_time int
+        , stats_reset int
+        , buffers_full int)
+);
+$$;
+
+
+ALTER PROCEDURE pgmetrics.refresh_wal_metrics() OWNER TO grafana;
+
+--
+-- TOC entry 2040 (class 1417 OID 16394)
 -- Name: pglog_server; Type: SERVER; Schema: -; Owner: postgres
 --
 
@@ -168,7 +197,7 @@ CREATE SERVER pglog_server FOREIGN DATA WRAPPER file_fdw;
 ALTER SERVER pglog_server OWNER TO postgres;
 
 --
--- TOC entry 213 (class 1259 OID 16395)
+-- TOC entry 212 (class 1259 OID 16395)
 -- Name: raw_metrics_id_seq; Type: SEQUENCE; Schema: pgmetrics; Owner: grafana
 --
 
@@ -189,7 +218,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 214 (class 1259 OID 16396)
+-- TOC entry 213 (class 1259 OID 16396)
 -- Name: raw_metrics; Type: TABLE; Schema: pgmetrics; Owner: grafana
 --
 
@@ -204,8 +233,8 @@ CREATE TABLE pgmetrics.raw_metrics (
 ALTER TABLE pgmetrics.raw_metrics OWNER TO grafana;
 
 --
--- TOC entry 3340 (class 0 OID 0)
--- Dependencies: 214
+-- TOC entry 3341 (class 0 OID 0)
+-- Dependencies: 213
 -- Name: COLUMN raw_metrics.cluster_name; Type: COMMENT; Schema: pgmetrics; Owner: grafana
 --
 
@@ -213,30 +242,69 @@ COMMENT ON COLUMN pgmetrics.raw_metrics.cluster_name IS 'Arbitrary name of the h
 
 
 --
--- TOC entry 215 (class 1259 OID 16402)
--- Name: activity_wait_event_type_counts; Type: TABLE; Schema: pgstats_history; Owner: grafana
+-- TOC entry 214 (class 1259 OID 16715)
+-- Name: wal_metrics; Type: TABLE; Schema: pgmetrics; Owner: grafana
 --
 
-CREATE TABLE pgstats_history.activity_wait_event_type_counts (
-    insert_timestamp timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    datid oid NOT NULL,
-    activity integer DEFAULT 0,
-    bufferpin integer DEFAULT 0,
-    client integer DEFAULT 0,
-    extension integer DEFAULT 0,
-    io integer DEFAULT 0,
-    ipc integer DEFAULT 0,
-    lock integer DEFAULT 0,
-    lwlock integer DEFAULT 0,
-    timeout integer DEFAULT 0
+CREATE TABLE pgmetrics.wal_metrics (
+    collection_at integer NOT NULL,
+    cluster_name text NOT NULL,
+    fpi integer,
+    sync integer,
+    bytes integer,
+    write integer,
+    records integer,
+    sync_time integer,
+    write_time integer,
+    stats_reset integer,
+    buffers_full integer
 )
-PARTITION BY RANGE (insert_timestamp);
+PARTITION BY RANGE (collection_at);
 
 
-ALTER TABLE pgstats_history.activity_wait_event_type_counts OWNER TO grafana;
+ALTER TABLE pgmetrics.wal_metrics OWNER TO grafana;
 
 --
--- TOC entry 3191 (class 2606 OID 16416)
+-- TOC entry 215 (class 1259 OID 16737)
+-- Name: wal_metrics_default; Type: TABLE; Schema: pgmetrics; Owner: grafana
+--
+
+CREATE TABLE pgmetrics.wal_metrics_default (
+    collection_at integer NOT NULL,
+    cluster_name text NOT NULL,
+    fpi integer,
+    sync integer,
+    bytes integer,
+    write integer,
+    records integer,
+    sync_time integer,
+    write_time integer,
+    stats_reset integer,
+    buffers_full integer
+);
+
+
+ALTER TABLE pgmetrics.wal_metrics_default OWNER TO grafana;
+
+--
+-- TOC entry 3183 (class 0 OID 0)
+-- Name: wal_metrics_default; Type: TABLE ATTACH; Schema: pgmetrics; Owner: grafana
+--
+
+ALTER TABLE ONLY pgmetrics.wal_metrics ATTACH PARTITION pgmetrics.wal_metrics_default DEFAULT;
+
+
+--
+-- TOC entry 3186 (class 2606 OID 16702)
+-- Name: raw_metrics raw_metrics_collection_at_cluster_name_key; Type: CONSTRAINT; Schema: pgmetrics; Owner: grafana
+--
+
+ALTER TABLE ONLY pgmetrics.raw_metrics
+    ADD CONSTRAINT raw_metrics_collection_at_cluster_name_key UNIQUE (collection_at, cluster_name);
+
+
+--
+-- TOC entry 3188 (class 2606 OID 16416)
 -- Name: raw_metrics raw_metrics_pkey; Type: CONSTRAINT; Schema: pgmetrics; Owner: grafana
 --
 
@@ -245,15 +313,41 @@ ALTER TABLE ONLY pgmetrics.raw_metrics
 
 
 --
--- TOC entry 3193 (class 2606 OID 16418)
--- Name: activity_wait_event_type_counts activity_wait_event_type_counts_pkey; Type: CONSTRAINT; Schema: pgstats_history; Owner: grafana
+-- TOC entry 3190 (class 2606 OID 16719)
+-- Name: wal_metrics wal_metrics_pkey; Type: CONSTRAINT; Schema: pgmetrics; Owner: grafana
 --
 
-ALTER TABLE ONLY pgstats_history.activity_wait_event_type_counts
-    ADD CONSTRAINT activity_wait_event_type_counts_pkey PRIMARY KEY (datid, insert_timestamp);
+ALTER TABLE ONLY pgmetrics.wal_metrics
+    ADD CONSTRAINT wal_metrics_pkey PRIMARY KEY (collection_at, cluster_name);
 
 
--- Completed on 2022-03-06 22:33:14 UTC
+--
+-- TOC entry 3192 (class 2606 OID 16741)
+-- Name: wal_metrics_default wal_metrics_default_pkey; Type: CONSTRAINT; Schema: pgmetrics; Owner: grafana
+--
+
+ALTER TABLE ONLY pgmetrics.wal_metrics_default
+    ADD CONSTRAINT wal_metrics_default_pkey PRIMARY KEY (collection_at, cluster_name);
+
+
+--
+-- TOC entry 3193 (class 0 OID 0)
+-- Name: wal_metrics_default_pkey; Type: INDEX ATTACH; Schema: pgmetrics; Owner: grafana
+--
+
+ALTER INDEX pgmetrics.wal_metrics_pkey ATTACH PARTITION pgmetrics.wal_metrics_default_pkey;
+
+
+--
+-- TOC entry 3194 (class 2606 OID 16720)
+-- Name: wal_metrics wal_metrics_collection_at_cluster_name_fkey; Type: FK CONSTRAINT; Schema: pgmetrics; Owner: grafana
+--
+
+ALTER TABLE pgmetrics.wal_metrics
+    ADD CONSTRAINT wal_metrics_collection_at_cluster_name_fkey FOREIGN KEY (collection_at, cluster_name) REFERENCES pgmetrics.raw_metrics(collection_at, cluster_name);
+
+
+-- Completed on 2022-03-20 19:29:31 UTC
 
 --
 -- PostgreSQL database dump complete
@@ -268,9 +362,9 @@ ALTER TABLE ONLY pgstats_history.activity_wait_event_type_counts
 --
 
 -- Dumped from database version 14.2 (Debian 14.2-1.pgdg110+1)
--- Dumped by pg_dump version 14.1
+-- Dumped by pg_dump version 14.2
 
--- Started on 2022-03-06 22:33:14 UTC
+-- Started on 2022-03-20 19:29:31 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -401,7 +495,7 @@ ALTER TABLE ONLY public.pgbench_tellers
     ADD CONSTRAINT pgbench_tellers_pkey PRIMARY KEY (tid);
 
 
--- Completed on 2022-03-06 22:33:14 UTC
+-- Completed on 2022-03-20 19:29:31 UTC
 
 --
 -- PostgreSQL database dump complete
@@ -418,9 +512,9 @@ ALTER TABLE ONLY public.pgbench_tellers
 --
 
 -- Dumped from database version 14.2 (Debian 14.2-1.pgdg110+1)
--- Dumped by pg_dump version 14.1
+-- Dumped by pg_dump version 14.2
 
--- Started on 2022-03-06 22:33:14 UTC
+-- Started on 2022-03-20 19:29:31 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -469,13 +563,13 @@ CREATE EXTENSION IF NOT EXISTS pgagent WITH SCHEMA pgagent;
 COMMENT ON EXTENSION pgagent IS 'A PostgreSQL job scheduler';
 
 
--- Completed on 2022-03-06 22:33:15 UTC
+-- Completed on 2022-03-20 19:29:31 UTC
 
 --
 -- PostgreSQL database dump complete
 --
 
--- Completed on 2022-03-06 22:33:15 UTC
+-- Completed on 2022-03-20 19:29:31 UTC
 
 --
 -- PostgreSQL database cluster dump complete
