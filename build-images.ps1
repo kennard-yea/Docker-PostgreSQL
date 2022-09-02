af42e3919905bc66a95633d51924e34e643ea933
kennard-yea <4001728+kennard-yea@users.noreply.github.com>
2022-09-02 13:16:02 -0400

    Removing reference to bsah-pgscripts

--- build-images.ps1
+++ build-images.ps1
@@ -1,7 +1,6 @@
 docker image build --tag internal/db1 db1
 docker image build --tag internal/db2 db2
 docker image build --tag internal/pgadmin pgadmin
-docker image build --tag internal/bash-pgscripts bash-pgscripts
 docker image build --tag internal/pgagent pgagent
 # docker image build --tag internal/pgmetrics pgmetrics
 docker image build --tag internal/pwsh-pgscripts pwsh-pgscripts
