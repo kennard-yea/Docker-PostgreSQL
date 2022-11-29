df9b1f052b36893101a5a8e11e319a3bb5a200d7
Christopher Dollinger <4001728+kennard-ygr@users.noreply.github.com>
2022-11-28 21:10:03 -0500

    Adding pgadmin user and database

--- primary-db-cluster/docker-entrypoint-initdb.d/2_pg_dumpall.sql
+++ primary-db-cluster/docker-entrypoint-initdb.d/2_pg_dumpall.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database cluster dump
 --
 
--- Started on 2022-05-22 16:14:20 UTC
+-- Started on 2022-11-29 02:00:10 UTC
 
 SET default_transaction_read_only = off;
 
@@ -14,8 +14,13 @@ SET standard_conforming_strings = on;
 --
 
 ALTER ROLE grafana WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS;
+ALTER ROLE pgadmin WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS;
 ALTER ROLE pgbench WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS;
 ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION BYPASSRLS;
+
+--
+-- User Configurations
+--
 
 
 --
@@ -23,6 +28,8 @@ ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION
 --
 
 GRANT pg_monitor TO grafana GRANTED BY postgres;
+
+
 
 
 
@@ -41,10 +48,10 @@ GRANT pg_monitor TO grafana GRANTED BY postgres;
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.3 (Debian 14.3-1.pgdg110+1)
--- Dumped by pg_dump version 14.2
+-- Dumped from database version 15.1 (Debian 15.1-1.pgdg110+1)
+-- Dumped by pg_dump version 15.1
 
--- Started on 2022-05-22 16:14:20 UTC
+-- Started on 2022-11-29 02:00:10 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -66,7 +73,7 @@ CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
 
 
 --
--- TOC entry 3320 (class 0 OID 0)
+-- TOC entry 3352 (class 0 OID 0)
 -- Dependencies: 2
 -- Name: EXTENSION pg_stat_statements; Type: COMMENT; Schema: -; Owner: 
 --
@@ -74,7 +81,7 @@ CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
 COMMENT ON EXTENSION pg_stat_statements IS 'track planning and execution statistics of all SQL statements executed';
 
 
--- Completed on 2022-05-22 16:14:20 UTC
+-- Completed on 2022-11-29 02:00:10 UTC
 
 --
 -- PostgreSQL database dump complete
@@ -88,10 +95,10 @@ COMMENT ON EXTENSION pg_stat_statements IS 'track planning and execution statist
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.3 (Debian 14.3-1.pgdg110+1)
--- Dumped by pg_dump version 14.2
+-- Dumped from database version 15.1 (Debian 15.1-1.pgdg110+1)
+-- Dumped by pg_dump version 15.1
 
--- Started on 2022-05-22 16:14:20 UTC
+-- Started on 2022-11-29 02:00:10 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -105,11 +112,11 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 3575 (class 1262 OID 16411)
+-- TOC entry 3606 (class 1262 OID 16417)
 -- Name: grafana; Type: DATABASE; Schema: -; Owner: grafana
 --
 
-CREATE DATABASE grafana WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.utf8';
+CREATE DATABASE grafana WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
 
 
 ALTER DATABASE grafana OWNER TO grafana;
@@ -128,7 +135,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 10 (class 2615 OID 16412)
+-- TOC entry 9 (class 2615 OID 16418)
 -- Name: pglogs; Type: SCHEMA; Schema: -; Owner: grafana
 --
 
@@ -138,7 +145,7 @@ CREATE SCHEMA pglogs;
 ALTER SCHEMA pglogs OWNER TO grafana;
 
 --
--- TOC entry 4 (class 3079 OID 16414)
+-- TOC entry 2 (class 3079 OID 16419)
 -- Name: file_fdw; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -146,8 +153,8 @@ CREATE EXTENSION IF NOT EXISTS file_fdw WITH SCHEMA public;
 
 
 --
--- TOC entry 3577 (class 0 OID 0)
--- Dependencies: 4
+-- TOC entry 3608 (class 0 OID 0)
+-- Dependencies: 2
 -- Name: EXTENSION file_fdw; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -155,7 +162,7 @@ COMMENT ON EXTENSION file_fdw IS 'foreign-data wrapper for flat file access';
 
 
 --
--- TOC entry 3 (class 3079 OID 16418)
+-- TOC entry 3 (class 3079 OID 16423)
 -- Name: pg_stat_statements; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -163,7 +170,7 @@ CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
 
 
 --
--- TOC entry 3578 (class 0 OID 0)
+-- TOC entry 3609 (class 0 OID 0)
 -- Dependencies: 3
 -- Name: EXTENSION pg_stat_statements; Type: COMMENT; Schema: -; Owner: 
 --
@@ -172,7 +179,7 @@ COMMENT ON EXTENSION pg_stat_statements IS 'track planning and execution statist
 
 
 --
--- TOC entry 2 (class 3079 OID 17890)
+-- TOC entry 4 (class 3079 OID 16454)
 -- Name: pgstattuple; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -180,8 +187,8 @@ CREATE EXTENSION IF NOT EXISTS pgstattuple WITH SCHEMA public;
 
 
 --
--- TOC entry 3579 (class 0 OID 0)
--- Dependencies: 2
+-- TOC entry 3610 (class 0 OID 0)
+-- Dependencies: 4
 -- Name: EXTENSION pgstattuple; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -189,7 +196,6 @@ COMMENT ON EXTENSION pgstattuple IS 'show tuple-level statistics';
 
 
 --
--- TOC entry 2184 (class 1417 OID 16446)
 -- Name: pglog_server; Type: SERVER; Schema: -; Owner: postgres
 --
 
@@ -198,24 +204,24 @@ CREATE SERVER pglog_server FOREIGN DATA WRAPPER file_fdw;
 
 ALTER SERVER pglog_server OWNER TO postgres;
 
--- Completed on 2022-05-22 16:14:21 UTC
+-- Completed on 2022-11-29 02:00:10 UTC
 
 --
 -- PostgreSQL database dump complete
 --
 
 --
--- Database "pgbench" dump
+-- Database "pgadmin" dump
 --
 
 --
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.3 (Debian 14.3-1.pgdg110+1)
--- Dumped by pg_dump version 14.2
+-- Dumped from database version 15.1 (Debian 15.1-1.pgdg110+1)
+-- Dumped by pg_dump version 15.1
 
--- Started on 2022-05-22 16:14:21 UTC
+-- Started on 2022-11-29 02:00:10 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -229,11 +235,108 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 3342 (class 1262 OID 16518)
+-- TOC entry 3362 (class 1262 OID 16800)
+-- Name: pgadmin; Type: DATABASE; Schema: -; Owner: pgadmin
+--
+
+CREATE DATABASE pgadmin WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
+
+
+ALTER DATABASE pgadmin OWNER TO pgadmin;
+
+\connect pgadmin
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 9 (class 2615 OID 16811)
+-- Name: pgadmin; Type: SCHEMA; Schema: -; Owner: pgadmin
+--
+
+CREATE SCHEMA pgadmin;
+
+
+ALTER SCHEMA pgadmin OWNER TO pgadmin;
+
+--
+-- TOC entry 2 (class 3079 OID 16386)
+-- Name: pg_stat_statements; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
+
+
+--
+-- TOC entry 3364 (class 0 OID 0)
+-- Dependencies: 2
+-- Name: EXTENSION pg_stat_statements; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION pg_stat_statements IS 'track planning and execution statistics of all SQL statements executed';
+
+
+--
+-- TOC entry 3 (class 3079 OID 16801)
+-- Name: pgstattuple; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pgstattuple WITH SCHEMA public;
+
+
+--
+-- TOC entry 3365 (class 0 OID 0)
+-- Dependencies: 3
+-- Name: EXTENSION pgstattuple; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION pgstattuple IS 'show tuple-level statistics';
+
+
+-- Completed on 2022-11-29 02:00:10 UTC
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- Database "pgbench" dump
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.1 (Debian 15.1-1.pgdg110+1)
+-- Dumped by pg_dump version 15.1
+
+-- Started on 2022-11-29 02:00:10 UTC
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- TOC entry 3373 (class 1262 OID 16465)
 -- Name: pgbench; Type: DATABASE; Schema: -; Owner: pgbench
 --
 
-CREATE DATABASE pgbench WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'en_US.utf8';
+CREATE DATABASE pgbench WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
 
 
 ALTER DATABASE pgbench OWNER TO pgbench;
@@ -252,7 +355,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 2 (class 3079 OID 16519)
+-- TOC entry 2 (class 3079 OID 16466)
 -- Name: pg_stat_statements; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -260,7 +363,7 @@ CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
 
 
 --
--- TOC entry 3343 (class 0 OID 0)
+-- TOC entry 3375 (class 0 OID 0)
 -- Dependencies: 2
 -- Name: EXTENSION pg_stat_statements; Type: COMMENT; Schema: -; Owner: 
 --
@@ -273,7 +376,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 214 (class 1259 OID 16743)
+-- TOC entry 218 (class 1259 OID 16497)
 -- Name: pgbench_accounts; Type: TABLE; Schema: public; Owner: pgbench
 --
 
@@ -289,7 +392,7 @@ WITH (fillfactor='100');
 ALTER TABLE public.pgbench_accounts OWNER TO pgbench;
 
 --
--- TOC entry 215 (class 1259 OID 16746)
+-- TOC entry 219 (class 1259 OID 16500)
 -- Name: pgbench_branches; Type: TABLE; Schema: public; Owner: pgbench
 --
 
@@ -304,7 +407,7 @@ WITH (fillfactor='100');
 ALTER TABLE public.pgbench_branches OWNER TO pgbench;
 
 --
--- TOC entry 212 (class 1259 OID 16737)
+-- TOC entry 220 (class 1259 OID 16503)
 -- Name: pgbench_history; Type: TABLE; Schema: public; Owner: pgbench
 --
 
@@ -321,7 +424,7 @@ CREATE TABLE public.pgbench_history (
 ALTER TABLE public.pgbench_history OWNER TO pgbench;
 
 --
--- TOC entry 213 (class 1259 OID 16740)
+-- TOC entry 221 (class 1259 OID 16506)
 -- Name: pgbench_tellers; Type: TABLE; Schema: public; Owner: pgbench
 --
 
@@ -337,7 +440,7 @@ WITH (fillfactor='100');
 ALTER TABLE public.pgbench_tellers OWNER TO pgbench;
 
 --
--- TOC entry 3193 (class 2606 OID 16758)
+-- TOC entry 3219 (class 2606 OID 16510)
 -- Name: pgbench_accounts pgbench_accounts_pkey; Type: CONSTRAINT; Schema: public; Owner: pgbench
 --
 
@@ -346,7 +449,7 @@ ALTER TABLE ONLY public.pgbench_accounts
 
 
 --
--- TOC entry 3195 (class 2606 OID 16754)
+-- TOC entry 3221 (class 2606 OID 16512)
 -- Name: pgbench_branches pgbench_branches_pkey; Type: CONSTRAINT; Schema: public; Owner: pgbench
 --
 
@@ -355,7 +458,7 @@ ALTER TABLE ONLY public.pgbench_branches
 
 
 --
--- TOC entry 3191 (class 2606 OID 16756)
+-- TOC entry 3223 (class 2606 OID 16514)
 -- Name: pgbench_tellers pgbench_tellers_pkey; Type: CONSTRAINT; Schema: public; Owner: pgbench
 --
 
@@ -363,7 +466,7 @@ ALTER TABLE ONLY public.pgbench_tellers
     ADD CONSTRAINT pgbench_tellers_pkey PRIMARY KEY (tid);
 
 
--- Completed on 2022-05-22 16:14:21 UTC
+-- Completed on 2022-11-29 02:00:11 UTC
 
 --
 -- PostgreSQL database dump complete
@@ -379,10 +482,10 @@ ALTER TABLE ONLY public.pgbench_tellers
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.3 (Debian 14.3-1.pgdg110+1)
--- Dumped by pg_dump version 14.2
+-- Dumped from database version 15.1 (Debian 15.1-1.pgdg110+1)
+-- Dumped by pg_dump version 15.1
 
--- Started on 2022-05-22 16:14:21 UTC
+-- Started on 2022-11-29 02:00:11 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -396,7 +499,7 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 2 (class 3079 OID 16563)
+-- TOC entry 2 (class 3079 OID 16515)
 -- Name: pg_buffercache; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -404,7 +507,7 @@ CREATE EXTENSION IF NOT EXISTS pg_buffercache WITH SCHEMA public;
 
 
 --
--- TOC entry 3447 (class 0 OID 0)
+-- TOC entry 3355 (class 0 OID 0)
 -- Dependencies: 2
 -- Name: EXTENSION pg_buffercache; Type: COMMENT; Schema: -; Owner: 
 --
@@ -413,7 +516,7 @@ COMMENT ON EXTENSION pg_buffercache IS 'examine the shared buffer cache';
 
 
 --
--- TOC entry 4 (class 3079 OID 16727)
+-- TOC entry 3 (class 3079 OID 16521)
 -- Name: pgstattuple; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -421,21 +524,21 @@ CREATE EXTENSION IF NOT EXISTS pgstattuple WITH SCHEMA public;
 
 
 --
--- TOC entry 3449 (class 0 OID 0)
--- Dependencies: 4
+-- TOC entry 3356 (class 0 OID 0)
+-- Dependencies: 3
 -- Name: EXTENSION pgstattuple; Type: COMMENT; Schema: -; Owner: 
 --
 
 COMMENT ON EXTENSION pgstattuple IS 'show tuple-level statistics';
 
 
--- Completed on 2022-05-22 16:14:22 UTC
+-- Completed on 2022-11-29 02:00:11 UTC
 
 --
 -- PostgreSQL database dump complete
 --
 
--- Completed on 2022-05-22 16:14:22 UTC
+-- Completed on 2022-11-29 02:00:11 UTC
 
 --
 -- PostgreSQL database cluster dump complete
