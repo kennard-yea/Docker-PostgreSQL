ccaf67ef09f12084d513afefbf15b6c49df26653
kennard-yea <4001728+kennard-yea@users.noreply.github.com>
2022-12-03 23:56:42 -0500

    Set up beaver user for future use

--- primary-db-cluster/docker-entrypoint-initdb.d/1_init_globals.sh
+++ primary-db-cluster/docker-entrypoint-initdb.d/1_init_globals.sh
@@ -29,9 +29,11 @@ function init_users() {
     createuser --echo --login "$PGBENCH_USER"
     createuser --echo --login "$GRAFANA_USER"
     createuser --echo --login "$PGADMIN_USER"
+    createuser --echo --login "$BEAVER_USER"
     psql --command="ALTER USER $PGBENCH_USER PASSWORD '$PGBENCH_PASSWORD'"
     psql --command="ALTER USER $GRAFANA_USER PASSWORD '$GRAFANA_PASSWORD'"
     psql --command="ALTER USER $PGADMIN_USER PASSWORD '$PGADMIN_PASSWORD'"
+    psql --command="ALTER USER $BEAVER_USER PASSWORD '$BEAVER_PASSWORD'"
     return 0
 }
 
