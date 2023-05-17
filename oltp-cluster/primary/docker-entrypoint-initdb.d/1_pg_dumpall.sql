071ca07023d56757b28b2ab9c10cbd4579e8ee25
kennard-ygr <4001728+kennard-ygr@users.noreply.github.com>
2023-05-16 22:25:14 -0400

    Refreshed pg_dumpall script

--- oltp-cluster/primary/docker-entrypoint-initdb.d/1_pg_dumpall.sql
+++ oltp-cluster/primary/docker-entrypoint-initdb.d/1_pg_dumpall.sql
@@ -2,8 +2,6 @@
 -- PostgreSQL database cluster dump
 --
 
--- Started on 2022-11-29 02:00:10 UTC
-
 SET default_transaction_read_only = off;
 
 SET client_encoding = 'UTF8';
@@ -20,9 +18,10 @@ ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION
 --
 
 
---
--- Role memberships
---
+
+
+
+
 
 
 --
@@ -39,10 +38,8 @@ ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 15.1 (Debian 15.1-1.pgdg110+1)
--- Dumped by pg_dump version 15.1
-
--- Started on 2022-11-29 02:00:10 UTC
+-- Dumped from database version 15.3 (Debian 15.3-1.pgdg110+1)
+-- Dumped by pg_dump version 15.3 (Debian 15.3-1.pgdg110+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -56,7 +53,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 2 (class 3079 OID 16386)
 -- Name: pg_stat_statements; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -64,15 +60,12 @@ CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
 
 
 --
--- TOC entry 3352 (class 0 OID 0)
--- Dependencies: 2
 -- Name: EXTENSION pg_stat_statements; Type: COMMENT; Schema: -; Owner: 
 --
 
 COMMENT ON EXTENSION pg_stat_statements IS 'track planning and execution statistics of all SQL statements executed';
 
 
--- Completed on 2022-11-29 02:00:10 UTC
 
 --
 -- PostgreSQL database dump complete
@@ -88,10 +81,8 @@ COMMENT ON EXTENSION pg_stat_statements IS 'track planning and execution statist
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 15.1 (Debian 15.1-1.pgdg110+1)
--- Dumped by pg_dump version 15.1
-
--- Started on 2022-11-29 02:00:11 UTC
+-- Dumped from database version 15.3 (Debian 15.3-1.pgdg110+1)
+-- Dumped by pg_dump version 15.3 (Debian 15.3-1.pgdg110+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -105,7 +96,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 2 (class 3079 OID 16515)
 -- Name: pg_buffercache; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -113,8 +103,6 @@ CREATE EXTENSION IF NOT EXISTS pg_buffercache WITH SCHEMA public;
 
 
 --
--- TOC entry 3355 (class 0 OID 0)
--- Dependencies: 2
 -- Name: EXTENSION pg_buffercache; Type: COMMENT; Schema: -; Owner: 
 --
 
@@ -122,7 +110,6 @@ COMMENT ON EXTENSION pg_buffercache IS 'examine the shared buffer cache';
 
 
 --
--- TOC entry 3 (class 3079 OID 16521)
 -- Name: pgstattuple; Type: EXTENSION; Schema: -; Owner: -
 --
 
@@ -130,21 +117,16 @@ CREATE EXTENSION IF NOT EXISTS pgstattuple WITH SCHEMA public;
 
 
 --
--- TOC entry 3356 (class 0 OID 0)
--- Dependencies: 3
 -- Name: EXTENSION pgstattuple; Type: COMMENT; Schema: -; Owner: 
 --
 
 COMMENT ON EXTENSION pgstattuple IS 'show tuple-level statistics';
 
 
--- Completed on 2022-11-29 02:00:11 UTC
 
 --
 -- PostgreSQL database dump complete
 --
-
--- Completed on 2022-11-29 02:00:11 UTC
 
 --
 -- PostgreSQL database cluster dump complete
